71f922087aac6fa21d4316f196ee5caf98478818
scheijan <scheijan@gmail.com>
2020-04-14 19:56:36 +0200

    initial version, hardcoded for field A1

---
+++ functions.py
@@ -0,0 +1,181 @@
+from time import sleep
+import os
+import random
+import logging
+
+import pyautogui as gui
+import pytesseract
+import cv2
+
+from constants import *
+
+log = logging.getLogger('main')
+
+
+def buildTower(pos):
+    gui.press('t')
+    gui.click(*pos)
+    gui.press('t')
+    sleep(STEP)
+
+
+def buildAmplifier(pos):
+    gui.press('a')
+    gui.click(*pos)
+    gui.press('a')
+    sleep(STEP)
+
+
+def buildLantern(pos):
+    gui.press('l')
+    gui.click(*pos)
+    gui.press('l')
+    sleep(STEP)
+
+
+def buildPylon(pos):
+    gui.press('p')
+    gui.click(*pos)
+    gui.press('p')
+    sleep(STEP)
+
+
+def buildTrap(pos):
+    gui.press('r')
+    gui.click(*pos)
+    gui.press('r')
+    sleep(STEP)
+
+
+def buildWall(pos):
+    gui.press('w')
+    gui.click(*pos)
+    gui.press('w')
+    sleep(STEP)
+
+
+def createGem(color, level=1):
+    gui.press(GEMKEYS.get(color, 'yellow'))
+    sleep(STEP)
+
+
+def upgradeGem(pos):
+    gui.moveTo(*pos)
+    gui.press('u')
+    sleep(STEP)
+
+
+def duplicateGem(pos):
+    gui.moveTo(*pos)
+    gui.press('d')
+    sleep(STEP)
+
+
+def sellGem(pos):
+    gui.moveTo(*pos)
+    gui.press('x')
+    sleep(STEP)
+
+
+def sellAllGems(pos):
+	for pos in INVENTORY:
+		sellGem(pos)
+
+
+def enrageWithGem(gemPosition):
+	gui.moveTo(*gemPosition)
+	gui.dragTo(*ENRAGESLOT)
+
+
+def enrageWithDefaultGem():
+	gui.click(*ENRAGESLOT)
+
+
+def addEnhancement(pos, enhancement):
+    gui.moveTo(*pos)
+    gui.press(str(enhancement))
+    sleep(STEP)
+
+
+def addRandomEnhancement(pos):
+    enhancement = random.randint(4, 6)
+    addEnhancement(pos, enhancement)
+
+
+def setPriority(gemPosition, priority):
+    if priority == 'flying':
+        gui.moveTo(*gemPosition)
+        gui.mouseDown(button="RIGHT")
+        gui.move(-25, 25)
+        gui.mouseUp(button="RIGHT")
+    else:
+        pass
+    sleep(STEP)
+
+
+def placeDefaultGemInBuilding(building):
+    gui.click(*building)
+    sleep(STEP)
+
+
+def placeGemInBuilding(gem, building):
+	gui.moveTo(*gem)
+	gui.dragTo(*building)
+	sleep(STEP)
+
+
+def backToTheMap():
+    gui.click(*BACKBUTTON)
+
+
+def startTime(fast=True):
+    gui.press('q')
+    if fast:
+        gui.press('q')
+    sleep(STEP)
+
+
+def togglePause():
+    gui.press('space')
+    sleep(STEP)
+
+
+def isRunning():
+    os.system('rm ~/gemcraft/screenshot*')
+    os.system('flameshot full -p ~/gemcraft/')
+    sleep(10)
+    img = cv2.imread('screenshot.png')
+    text = pytesseract.image_to_string(img)
+    # log.info(text)
+    words = ['Field', 'Journey', 'mode', 'highest', 'Gathered',
+             'Longest', 'Shrines', 'Activated', 'Waves', 'Called', 'Early']
+    for word in words:
+        if word in text:
+            return False
+
+    return True
+
+
+def startField(field, center, run, label='unknown'):
+    log.info('starting field "%s" at position %s' % (label, field))
+    # click on the field
+    if run == 1:
+        gui.click(*field)
+        gui.click(*field)
+    else:
+        gui.click(*center)
+        gui.click(*center)
+		gui.click(*field)
+		gui.click(*field)
+    sleep(1)
+
+    # click on Start the Battle!
+    gui.click(*STARTBUTTON)
+
+    # wait for field to load
+    if run == 1:
+        sleep(15)
+    else:
+        sleep(8)
+
+
---
+++ gemcraft.py
@@ -0,0 +1,80 @@
+from time import sleep
+from datetime import datetime
+import logging
+
+import coloredlogs
+
+from constants import *
+from functions import *
+
+
+
+FIELD = (803, 277)
+CENTERFIELD = (961, 567)
+
+TOWERS = [(255, 193), (495, 593), (441, 647),
+          (491, 645), (547, 647), (493, 701)]
+
+AMPLIFIERS = [(439, 591), (541, 591), (441, 699), (547, 699)]
+
+BUILDINGS = TOWERS + AMPLIFIERS
+
+
+
+log = logging.getLogger('main')
+coloredlogs.install(
+    level='INFO', fmt='[%(asctime)s] %(levelname)s %(message)s', datefmt='%d/%b/%Y %I:%M:%S')
+
+
+def main(run):
+    log.info('starting run %s at %s' % (run, datetime.now()))
+
+    startField(FIELD, CENTERFIELD, run)
+
+    # build 4 amps
+    log.info('building %s amplifier' % len(AMPLIFIERS))
+    for amp in AMPLIFIERS:
+        buildAmplifier(amp)
+
+    # build 5 + 1 towers
+    log.info('building %s towers' % len(TOWERS))
+    for tower in TOWERS:
+        buildTower(tower)
+
+    # create yellow gems in all buildings
+    log.info('creating and placing %s gems' % (len(BUILDINGS)))
+    for building in BUILDINGS:
+        createGem('yellow')
+        placeDefaultGemInBuilding(building)
+
+    log.info('setting all priorities to "flying"')
+    for tower in TOWERS:
+        setPriority(tower, 'flying')
+
+    # start he game
+    log.info('starting the game')
+    startTime(fast=True)
+
+    # loop: keep upgrading everything
+    upgradeCounter = 0
+    while True:
+        upgradeCounter += 1
+        log.info('upgrade round %s' % upgradeCounter)
+        for building in BUILDINGS:
+            upgradeGem(building)
+            if building in TOWERS:
+                addRandomEnhancement(building)
+            sleep(1)
+
+        if not isRunning():
+            log.info('game over?')
+            backToTheMap()
+            sleep(10)
+            break
+        log.info('game is still on')
+
+if __name__ == '__main__':
+    run = 0
+    while True:
+        run += 1
+        main(run)
